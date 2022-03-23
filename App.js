e9cac130a90206bd555e843a4f94bce4c7f3ea90
Carlos Alves <carlosaalves07@outlook.com>
2022-03-23 15:24:13 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
+
+export default function App() {
+
+  const [nome, SetNome] = useState('');
+  const [email, SetEmail] = useState('');
+  const [senha, SetSenha] = useState('');
+
+  const cadastro =() => {
+    alert(senha)
+    alert(email)
+    alert(nome)
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden />
+
+      
+      <Image style={{width:300, height:200}} source={require('./assets/react-native.png')}/>
+     
+
+      <TextInput placeholder='Digite seu nome' style={styles.textInput} onChangeText={text=>SetNome(text)}/>
+      <TextInput placeholder='Digite seu e-mail' style={styles.textInput} onChangeText={text=>SetEmail(text)}/>
+      <TextInput secureTextEntry={true} placeholder='Digite sua senha' style={styles.textInput} onChangeText={text=>SetSenha(text)}/>
+
+      <TouchableOpacity onPress={()=>cadastro()}>
+        <Text >CADASTRAR!</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#29c6e2',
+    
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding:20
+  },
+
+textInput: {
+  width:'100%',
+  height:40,
+  backgroundColor:'white',
+  borderRadius:20,
+  paddingLeft:10,
+  marginBottom: 10
+}
+
+ 
+});
